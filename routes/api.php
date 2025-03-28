a565635741462a7afafefec2d8a2ada007b337fc
Pushpesh Sharma <pushpesh47@gmail.com>
2025-03-28 14:01:39 +0530

    added settings api routes

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,15 @@ Route::prefix('customer')->group(function () {
             Route::post('create-new-request',[MyRequestController::class,'createNewRequest']);
             Route::post('add-image-to-submitted-request',[MyRequestController::class,'addImageToSubmittedRequest']);
             Route::post('add-details-to-request',[MyRequestController::class,'addDetailsToRequest']);
+            Route::post('check-paragraph-quality',[MyRequestController::class,'checkParagraphQuality']);
+        });
+
+        Route::prefix('setting')->group(function () {       
+
+            Route::get('get-profile-info',[AccountSettingController::class,'getProfileInfo']);
+            Route::get('update-profile-image',[AccountSettingController::class,'updateProfileImage']);
+            Route::post('update-profile-info',[AccountSettingController::class,'updateProfileInfo']);
+            Route::post('change-password',[AccountSettingController::class,'changePassword']);
         });
 
     });
