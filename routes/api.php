ca6da036b7709e862ff8fbd522fa1727cdf94214
deepaliwps3 <151629752+deepaliwps3@users.noreply.github.com>
2025-05-14 16:33:30 +0530

    created new api

--- routes/api.php
+++ routes/api.php
@@ -140,7 +140,7 @@ Route::prefix('users')->group(function () {
         Route::post('/add-manual-bid', [RecommendedLeadsController::class, 'addManualBid']);
         Route::post('/autobid', [RecommendedLeadsController::class, 'addRecommendedLeads']);
         Route::post('/sort-by-location', [RecommendedLeadsController::class, 'sortByLocation']);
-        // Route::post('/sort-by-responsetime', [RecommendedLeadsController::class, 'sortByResponsetime']);
+        Route::post('/response-time-filter', [RecommendedLeadsController::class, 'responseTimeFilter']);
         
 
         Route::post('/buy-credits', [CreditPlanController::class, 'buyCredits']);
