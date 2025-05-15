4dbf2ad417a63a1d289c0b03bc4132b95c6c0650
deepaliwps3 <151629752+deepaliwps3@users.noreply.github.com>
2025-05-15 19:03:32 +0530

    created new api

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class ActivityLog extends Model
 {
     protected $fillable = ['lead_id','from_user_id','to_user_id','activity_name','contact_type','duration','duration_minutes'];
+
+    public function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
