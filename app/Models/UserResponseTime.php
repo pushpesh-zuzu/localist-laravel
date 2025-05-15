e247ea96823694ecdecc602fdf5edf536523f619
deepaliwps3 <151629752+deepaliwps3@users.noreply.github.com>
2025-05-15 20:05:22 +0530

    created new api

--- app/Models/UserResponseTime.php
+++ app/Models/UserResponseTime.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class UserResponseTime extends Model
 {
     protected $fillable = ['seller_id','minimum_duration','average'];
 }
+public function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
