d7e016bf07b13b4dc33a8684ad07bfb2dae02f99
deepaliwps3 <151629752+deepaliwps3@users.noreply.github.com>
2025-06-03 17:05:56 +0530

    created api for menus

--- app/Http/Controllers/Api/PagesController.php
+++ app/Http/Controllers/Api/PagesController.php
@@ -21,7 +21,6 @@ class PagesController extends Controller
         $forProfessionalsMenus = ['How it works', 'Pricing', 'Join as a Professional', 'Help Centre', 'Mobile App'];
         $aboutMenus = ['About Localists', 'Careers', 'Blog', 'Press'];
 
-        // Fetching grouped menu items by menu_name
         $forCustomers = Menu::whereIn('menu_name', $forCustomersMenus)->get();
         $forProfessionals = Menu::whereIn('menu_name', $forProfessionalsMenus)->get();
         $about = Menu::whereIn('menu_name', $aboutMenus)->get();
@@ -39,7 +38,6 @@ class PagesController extends Controller
 
     public function pageDetails($page_slug){
         $pageDetails = Page::where('slug', $page_slug)->get();
-        
         return $this->sendResponse(__('Pages Data'), ['pageDetails' => $pageDetails]);
     }
 }
