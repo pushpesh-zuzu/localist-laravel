f6a0cecd1a893d3d6c58201dec2b14dd9fe4bbcf
deepaliwps3 <151629752+deepaliwps3@users.noreply.github.com>
2025-06-05 13:00:34 +0530

    changes in autobid api

--- app/Http/Controllers/Api/RecommendedLeadsController.php
+++ app/Http/Controllers/Api/RecommendedLeadsController.php
@@ -616,12 +616,24 @@ class RecommendedLeadsController extends Controller
     
         $sellersWith3Bids = [];
         if ($applySellerLimit) {
-            $sellersWith3Bids = RecommendedLead::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
-                ->select('seller_id')
-                ->groupBy('seller_id')
-                ->havingRaw('COUNT(DISTINCT buyer_id) >= 3')
-                ->pluck('seller_id')
-                ->toArray();
+            $sellersWith3Bids = RecommendedLead::select('seller_id', DB::raw('MIN(created_at) as first_bid_date'))
+                                                ->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+                                                ->groupBy('seller_id')
+                                                ->havingRaw('COUNT(DISTINCT buyer_id) >= 3')
+                                                ->get()
+                                                ->filter(function ($record) {
+                                                    $autobidDaysLimit = CustomHelper::setting_value('autobid_days_limit', 0); // Use your config
+                                                    return Carbon::parse($record->first_bid_date)->diffInDays(Carbon::now()) < $autobidDaysLimit;
+                                                })
+                                                ->pluck('seller_id')
+                                                ->toArray();
+
+            // $sellersWith3Bids = RecommendedLead::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+            //     ->select('seller_id')
+            //     ->groupBy('seller_id')
+            //     ->havingRaw('COUNT(DISTINCT buyer_id) >= 3')
+            //     ->pluck('seller_id')
+            //     ->toArray();
         }
     
         $responseTimesMap = DB::table('user_response_times')
