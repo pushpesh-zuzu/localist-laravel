fba0b6fdbcb1b8aaa871c1ce901167053b9e9b27
Pushpesh Sharma <pushpesh47@gmail.com>
2025-06-24 23:12:11 +0530

    updated autobid-list added param activity_log - 3

--- app/Http/Controllers/Api/RecommendedLeadsController.php
+++ app/Http/Controllers/Api/RecommendedLeadsController.php
@@ -124,7 +124,7 @@ class RecommendedLeadsController extends Controller
 
                     $sellerData['activty_log'] = [
                         'log' => !empty($firstResponse->activity_name) ? $firstResponse->activity_name : 'Requested a Quote',
-                        'date_time' => !empty($firstResponse->created_at) ? date('d M Y, H:i', strtotime($firstResponse->created_at)) : date('d M Y, H:i')
+                        'date_time' => !empty($firstResponse->created_at) ? date('d M Y, H:i', strtotime($bid->created_at)) : date('d M Y, H:i')
                     ];
                     $result[] = $sellerData;
                 }
