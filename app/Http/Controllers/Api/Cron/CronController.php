2379d29bc6dcff944e3709a6c207996b48776651
athirababu <athirakottara@gmail.com>
2025-07-28 16:02:56 +0530

    updation in email

--- app/Http/Controllers/Api/Cron/CronController.php
+++ app/Http/Controllers/Api/Cron/CronController.php
@@ -244,7 +244,7 @@ class CronController extends Controller
                         $alreadySent = EmailLog::where('user_id', $seller->id)
                             ->where('lead_id', $lead->id)
                             ->whereDate('created_at', Carbon::today())
-                            ->where('setting_name', 'Send New Lead Request Email')
+                            ->where('setting_name', 'New Lead - Auto Bid Enabled- With Credits')
                             ->exists();
 
 
