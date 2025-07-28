306223721fb45c165ed0e1ed1d0a128f512e08ae
athirababu <athirakottara@gmail.com>
2025-07-28 15:04:00 +0530

    update in zoho email

--- app/Helpers/Zoho/ZohoEmails.php
+++ app/Helpers/Zoho/ZohoEmails.php
@@ -319,7 +319,7 @@ class ZohoEmails
     public static function sendLeadEmailBidEnough($userId, $leadId)
     {
 
-        $sendLeadRequestEmail = EmailSetting::where('setting_name', 'Send New Lead Request Email')->value('setting_value');
+        $sendLeadRequestEmail = EmailSetting::where('setting_name', 'New Lead - Auto Bid Enabled- With Credits')->value('setting_value');
 
         if ($sendLeadRequestEmail) {
             $accessToken = ZohoHelper::getAccessToken();
@@ -378,7 +378,7 @@ class ZohoEmails
 
                     $fromEmail = CustomHelper::setting_value('zoho_default_from_email', 'mikemarshall402@hotmail.com');
                     $toEmail = $user->email;
-                    $subject = 'New lead opportunity just for you!';
+                    $subject = 'New Lead Secured – Auto Bid Active & Contact Details Inside';
 
                     $response = Http::withToken($accessToken)
                         ->post($url, [
@@ -408,7 +408,7 @@ class ZohoEmails
                     $dataE['to_email'] = $toEmail;
                     $dataE['message_id'] = $rel['message_id'];
                     $dataE['subject'] = $subject;
-                    $dataE['setting_name'] = 'Send New Lead Request Email';
+                    $dataE['setting_name'] = 'New Lead - Auto Bid Enabled- With Credits';
                     $dataE['content'] = $htmlContent;
                     $dataE['zoho_url'] = $url;
                     $dataE['response'] = json_encode($rel);
