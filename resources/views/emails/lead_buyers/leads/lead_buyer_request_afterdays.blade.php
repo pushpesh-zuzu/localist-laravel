eb793ccfd438f0b410b9c52d1e04c8b741eabcdd
athirababu <athirakottara@gmail.com>
2025-07-29 19:24:41 +0530

    updation in api

--- resources/views/emails/lead_buyers/leads/lead_buyer_request_afterdays.blade.php
+++ resources/views/emails/lead_buyers/leads/lead_buyer_request_afterdays.blade.php
@@ -32,7 +32,7 @@
               @foreach ($leadDataList as $lead)
               <div style="margin-top: 16px; background-color: #f5f9fc; padding: 16px; border-radius: 4px; font-size: 16px; line-height: 24px;">
                 <strong>📊 Total Jobs:</strong> {{ $lead['count'] }}<br>
-                <strong>💼 Job Value:</strong> ₹{{ $lead['credit_sum'] }}<br>
+                <strong>💼 Job Value:</strong> £{{ $lead['credit_sum'] }}<br>
                 <strong>📍 Location:</strong> {{ ucfirst($lead['area']) }}<br>
                 <strong>🛠 Service:</strong> {{ $lead['category_name'] }}
               </div>
