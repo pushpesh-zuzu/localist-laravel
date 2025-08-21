d72f0f03f44228220ab41c6ee60577324f332f41
Pushpesh Sharma <pushpesh47@gmail.com>
2025-08-21 15:34:16 +0530

    git check in dev deploy-5

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/check_api', function () {
-    return "api is working!";
+    return "api is working!.";
 });
 
 Route::post('/contact-us', [ContactUsController::class, 'store']);
