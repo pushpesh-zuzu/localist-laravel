49541851a8efdaa57407c7c91ce0c8608325bdbd
Pushpesh Sharma <pushpesh47@gmail.com>
2025-08-23 17:14:52 +0530

    updated auth urls

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('/check-mail', function() {
 // })->middleware(['auth:admin', 'verified'])->name('dashboard');
 // Route::get('/', [DashboardController::class, 'index'])->middleware(['auth:admin', 'verified'])->name('dashboard');
 Route::middleware('auth:admin')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->middleware(['auth:admin'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth:admin'])->name('dashboard');
     Route::get('/users/{type?}', [UserController::class, 'index'])->name('user.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
