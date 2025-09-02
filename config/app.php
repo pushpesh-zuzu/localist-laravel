52127c6e2b8b35dad518a58424fadffa4fca1070
Pushpesh <pushpeshsh@zuzucodes.com>
2025-09-02 20:33:42 +0530

    changed timezone to Europe/London in app.config

--- config/app.php
+++ config/app.php
@@ -73,7 +73,7 @@ return [
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | is set to "UTC" by default as it is suitable for most use cases. 
     |
     */
 
