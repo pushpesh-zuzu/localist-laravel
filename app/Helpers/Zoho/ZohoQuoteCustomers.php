8767f07b3d5f2ac73506b37aa2a9618fa074ef45
athirababu <athira@zuzucodes.com>
2025-09-15 17:00:16 +0530

    updation in zoho quote

--- app/Helpers/Zoho/ZohoQuoteCustomers.php
+++ app/Helpers/Zoho/ZohoQuoteCustomers.php
@@ -21,7 +21,14 @@ class ZohoQuoteCustomers
         }
 
         //$zohoId = $this->getZohoCustomerId($access_token, $user->id);
+        if (!$type) {
+            $email = $payload['data'][0]['Email'] ?? null;
+            if ($email) {
+                $this->deleteAbandonedIfExists($access_token, $email);
+            }
+        }
         $payload = $this->buildCustomerPayload($userId,$type);
+
         $response = $this->upsertToZohoService($access_token, $payload);
 
         $responseData = $response->json();
@@ -83,6 +90,35 @@ class ZohoQuoteCustomers
             ->post('https://www.zohoapis.eu/crm/v2/Quote_Customers/upsert', $payload);
 
     }
+
+    protected function deleteAbandonedIfExists($accessToken, $email)
+{
+    // 1. Search abandoned record by email
+    $searchUrl = "https://www.zohoapis.eu/crm/v2/Quote_Customers/search?email=" . urlencode($email);
+    $response = Http::withToken($accessToken)->get($searchUrl);
+
+    if ($response->successful() && isset($response['data'])) {
+        foreach ($response['data'] as $record) {
+            if (
+                isset($record['Email']) &&
+                strtolower($record['Email']) === strtolower($email) &&
+                isset($record['registration_type']) &&
+                $record['registration_type'] === 'abandoned'
+            ) {
+                // 2. Delete the abandoned record
+                $recordId = $record['id'];
+                Http::withToken($accessToken)
+                    ->delete("https://www.zohoapis.eu/crm/v2/Quote_Customers/$recordId");
+
+                Log::info("Deleted abandoned Zoho Quote_Customer", [
+                    'email' => $email,
+                    'record_id' => $recordId
+                ]);
+            }
+        }
+    }
+}
+
 
 
 }
