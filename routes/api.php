dfcb7a882ab53c9ba7e83beb890bff7f8a25ef00
Pushpesh <pushpeshsh@zuzucodes.com>
2025-10-01 23:55:43 +0530

    google check 6

--- routes/api.php
+++ routes/api.php
@@ -103,8 +103,6 @@ Route::prefix('payment')->group(function () {
 });
 
 
-Route::post('auth/callback',[GoogleAuthController::class,'getAuthToken']);
-
 Route::prefix('google')->group(function () {
     Route::post('get-auth-token',[GoogleAuthController::class,'getAuthToken']);
     
