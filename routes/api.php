a7b0380aa99a6e7d554b2153fbcf3d26617f0aeb
Pushpesh <pushpeshsh@zuzucodes.com>
2026-02-05 19:22:50 +0530

    added fb landscaping form

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\Api\OctoparseController;
 use App\Http\Controllers\Api\Facebook\Forms\DrivewayInstallationForm;
 use App\Http\Controllers\Api\Facebook\Forms\RoofingForm;
 use App\Http\Controllers\Api\Facebook\Forms\FenceAndGateForm;
+use App\Http\Controllers\Api\Facebook\Forms\LandscapingForm;
 
 // use App\Http\Controllers\Api\ZohoController;
 
@@ -31,6 +32,7 @@ use App\Http\Controllers\Api\Facebook\Forms\FenceAndGateForm;
 Route::post('get-facebook-leads-driveway-installation-from',[DrivewayInstallationForm::class,'getFacebookLeadsDrivewayInstallationFrom']);
 Route::post('get-facebook-leads-roofing-from',[RoofingForm::class,'getFacebookLeadsRoofingFrom']);
 Route::post('get-facebook-leads-fence-and-gate-from',[FenceAndGateForm::class,'getFacebookLeadsFenceAndGateForm']);
+Route::post('get-facebook-leads-landscaping-from',[LandscapingForm::class,'getFacebookLeadsLandscapingForm']);
 
 Route::post('get-facebook-leads-tt',[DrivewayInstallationForm::class,'tt']);
 
