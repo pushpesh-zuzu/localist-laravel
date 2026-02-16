01ed4b7f0300636a2a443ec8af9a9e1c387183de
ashish <ashish.gupta052@gmail.com>
2026-02-16 19:13:14 +0530

    Updated phone field on lead purchased module on zoho crm

--- app/Helpers/Zoho/ZohoPurchasedLeads.php
+++ app/Helpers/Zoho/ZohoPurchasedLeads.php
@@ -119,7 +119,7 @@ class ZohoPurchasedLeads
                 'Lead_Post_Code'           => (string) ($postcode  ?? ''),
                 'Customer_Email'           => (string) ($userEmail ?? ''),
                 'Customer_Phone'           => (string) ($userPhone ?? ''),
-                'Lead_Buyer_Phone'         =>  (string) ($LeadBuyerPhone ?? ''),
+                'Phone'         =>  (string) ($LeadBuyerPhone ?? ''),
                 'Lead_Buyer_Name'          =>  (string) ($LeadBuyerName ?? '')
                 
 
