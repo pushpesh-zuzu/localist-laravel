7065d66d7bedd4673b15225a7a58cac03eafd48a
Pushpesh <pushpeshsh@zuzucodes.com>
2026-04-20 17:49:01 +0530

    updated add/edit service questions to accept 0 scoring 2

--- resources/views/servicequestion/create.blade.php
+++ resources/views/servicequestion/create.blade.php
@@ -75,7 +75,7 @@
                     <input type="text" class="form-control" placeholder="Question Option" name="ques_opt[]" id="ques_opt" required>
                 </div>
                 <div class="col-md-2 d-flex align-items-center gap-2">
-                    <input type="number" class="form-control" placeholder="Score" name="ques_score[]" id="ques_score" min="1" max="50" required>
+                    <input type="number" class="form-control" placeholder="Score" name="ques_score[]" id="ques_score" min="0" max="100" required>
                 </div>
                 <div class="col-md-2 d-flex align-items-center gap-2">
                     <input type="text" class="form-control" placeholder="Next Question Number" name="next_ques[]" id="next_ques" pattern="^\d+$|^last$" title="Enter a number or the word 'last'" required>
