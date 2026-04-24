627ce113c5540d6343e894c67d5a31f6b138825b
Pushpesh <pushpeshsh@zuzucodes.com>
2026-04-24 16:42:46 +0530

    updated 0 score for add more options in service questions

--- resources/views/servicequestion/add-more-option.blade.php
+++ resources/views/servicequestion/add-more-option.blade.php
@@ -4,7 +4,7 @@
        <input type="text" class="form-control" placeholder="Question Option" name="ques_opt[]" id="ques_opt{{$count+1}}" required>
     </div>
     <div class="col-md-2 d-flex align-items-center gap-2">
-        <input type="number" class="form-control" placeholder="Score" name="ques_score[]" id="ques_score{{$count+1}}" min="1" max="50" required>
+        <input type="number" class="form-control" placeholder="Score" name="ques_score[]" id="ques_score{{$count+1}}" min="0" max="100" required>
     </div>
     <div class="col-md-2 d-flex align-items-center gap-2">
         <input type="text" class="form-control" placeholder="Next Question Number" name="next_ques[]" id="next_ques{{$count+1}}" pattern="^\d+$|^last$" title="Enter a number or the word 'last'" required>
