c1c0483e2c4b3c4553a5fe8511de042bc373f419
ashish <ashish.gupta052@gmail.com>
2026-05-04 13:47:48 +0530

    again Add 2/3 lead bid status

--- app/Http/Controllers/Api/LeadPreferenceController.php
+++ app/Http/Controllers/Api/LeadPreferenceController.php
@@ -2277,7 +2277,7 @@ class LeadPreferenceController extends Controller
          $totalBids = RecommendedLead::where('lead_id', $lead->lead_id)          
             ->count();
      
-        $lead->bid_status = $totalBids . '/' . $leadSlotCount;
+        $lead->bid_status = (string) $totalBids . '/' . $leadSlotCount;
               
 
         return $lead;
